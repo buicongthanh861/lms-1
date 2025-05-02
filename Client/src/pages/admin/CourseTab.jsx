4b85336e45d3e76b2b28e8181f3b9a44149bf0db
buicongthanh861 <buicongthanh861@gmail.com>
2025-05-03 03:36:55 +0700

    Update CourseTab.jsx

--- Client/src/pages/admin/CourseTab.jsx
+++ Client/src/pages/admin/CourseTab.jsx
@@ -1,8 +1,14 @@
-import RichTextEditor from '@/components/RichTextEditor'
-import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import RichTextEditor from "@/components/RichTextEditor";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectContent,
@@ -11,123 +17,159 @@ import {
   SelectLabel,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
-import { setCourse } from '@/redux/courseSlice'
-import axios from 'axios'
-import { Loader2 } from 'lucide-react'
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { useNavigate, useParams } from 'react-router-dom'
-import { toast } from 'sonner'
+} from "@/components/ui/select";
+import { setCourse } from "@/redux/courseSlice";
+import axios from "axios";
+import { Loader2 } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "sonner";
 
 const CourseTab = () => {
-  const params = useParams()
-  const id = params.courseId
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const {course} = useSelector(store => store.course)
-  const selectCourse = course.find(course => course._id === id)
+  const params = useParams();
+  const id = params.courseId;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { course } = useSelector((store) => store.course);
+  const selectCourse = course.find((course) => course._id === id);
 
-  const [selectedCourse,setSelectedCourse] = useState(selectCourse)
-  const [loading,setLoading] = useState(false)
-  const {publish,setPublish} = useState(false)
+  const [selectedCourse, setSelectedCourse] = useState(selectCourse);
+  const [loading, setLoading] = useState(false);
+  const { publish, setPublish } = useState(false);
 
-  const getCourseById = async()=> {
+  const getCourseById = async () => {
     try {
-      const res = await axios.get(`http://localhost:8000/api/v1/course/${id}` , {withCredentials:true})
-      if(res.data.success){
-        setSelectedCourse(res.data.course)
+      const res = await axios.get(`http://localhost:8000/api/v1/course/${id}`, {
+        withCredentials: true,
+      });
+      if (res.data.success) {
+        setSelectedCourse(res.data.course);
       }
     } catch (error) {
       console.log(error);
-      
     }
-  }
-  useEffect(()=> {
-    getCourseById()
-  })
+  };
+  useEffect(() => {
+    getCourseById();
+  });
 
-  const [input,setInput] = useState({
-      courseTitle:selectedCourse?.courseTitle,
-      subTitle:selectedCourse?.subTitle,
-      description:selectedCourse?.description,
-      category:selectedCourse?.category,
-      courseLevel:selectedCourse?.courseLevel,
-      coursePrice:selectedCourse?.coursePrice,
-      file:""
-  })
-  const [previewThumbnail,setPreviewThumbnail] = useState(selectedCourse?.Thumbnail)
+  const [input, setInput] = useState({
+    courseTitle: selectedCourse?.courseTitle,
+    subTitle: selectedCourse?.subTitle,
+    description: selectedCourse?.description,
+    category: selectedCourse?.category,
+    courseLevel: selectedCourse?.courseLevel,
+    coursePrice: selectedCourse?.coursePrice,
+    file: "",
+  });
+  const [previewThumbnail, setPreviewThumbnail] = useState(
+    selectedCourse?.Thumbnail
+  );
 
   const changeEventHandler = (e) => {
-    const {name,value} = e.target;
-    setInput({...input,[name] : value})
-  }
-  const selectCategory = (value)=> {
-      setInput({...input,category:value})
-  }
+    const { name, value } = e.target;
+    setInput({ ...input, [name]: value });
+  };
+  const selectCategory = (value) => {
+    setInput({ ...input, category: value });
+  };
 
-  const selectCourseLevel = (value)=> {
-    setInput({...input,courseLevel:value})
-  }
+  const selectCourseLevel = (value) => {
+    setInput({ ...input, courseLevel: value });
+  };
 
   //get file
   const selectThumbnail = (e) => {
     const file = e.target.files?.[0];
-    if(file){
-      setInput({...input,courseThumbnail:file});
-      const fileReader = new FileReader()
+    if (file) {
+      setInput({ ...input, courseThumbnail: file });
+      const fileReader = new FileReader();
       fileReader.onload = () => setPreviewThumbnail(fileReader.result);
-      fileReader.readAsDataURL(file)
+      fileReader.readAsDataURL(file);
     }
-  }
+  };
 
-  const updateCourseHandler = async()=> {
+  const updateCourseHandler = async () => {
     const formData = new FormData();
-    formData.append("courseTitle",input.courseTitle);
-    formData.append("subTitle",input.subTitle);
-    formData.append("description",input.description);
-    formData.append("category",input.category);
-    formData.append("courseLevel",input.courseLevel);
-    formData.append("coursePrice",input.coursePrice);
-    formData.append("file",input.courseThumbnail);
+    formData.append("courseTitle", input.courseTitle);
+    formData.append("subTitle", input.subTitle);
+    formData.append("description", input.description);
+    formData.append("category", input.category);
+    formData.append("courseLevel", input.courseLevel);
+    formData.append("coursePrice", input.coursePrice);
+    formData.append("file", input.courseThumbnail);
 
     try {
-      setLoading(true)
-      const res = await axios.put(`http://localhost:8000/api/v1/course/${id}` ,formData,{
-        headers: {
-          'Content-Type' : "multipart/form-data"
-        },
-        withCredentials:true
-      })
-      if(res.data.success){
-        navigate(`lecture`)
-        toast.success(res.data.message)
-        dispatch([...course,setCourse(res.data.course)])
+      setLoading(true);
+      const res = await axios.put(
+        `http://localhost:8000/api/v1/course/${id}`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          withCredentials: true,
+        }
+      );
+      if (res.data.success) {
+        navigate(`lecture`);
+        toast.success(res.data.message);
+        dispatch([...course, setCourse(res.data.course)]);
       }
     } catch (error) {
       console.log(error);
-
-    }finally{
-      setLoading(false)
+    } finally {
+      setLoading(false);
     }
-  }
+  };
+  const deleteCourseHandler = async () => {
+    if (
+      !window.confirm(
+        "Are you sure you want to delete this course? This action cannot be undone."
+      )
+    )
+      return;
 
-  const togglePublishUnpublish = async(action)=> {
     try {
-      const res = await axios.patch(`http://localhost:8000/api/v1/course/${id}`,{
-        params:{
-          action
-        },
-        withCredentials:true
-      })
-      if(res.data.success){
-        setPublish(!publish)
-        toast.success(res.data.message)
+      setLoading(true);
+      const res = await axios.delete(
+        `http://localhost:8000/api/v1/course/${id}`,
+        {
+          withCredentials: true,
+        }
+      );
+      if (res.data.success) {
+        toast.success(res.data.message);
+        navigate("/admin/course");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Failed to delete course");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const togglePublishUnpublish = async (action) => {
+    try {
+      const res = await axios.patch(
+        `http://localhost:8000/api/v1/course/${id}`,
+        {
+          params: {
+            action,
+          },
+          withCredentials: true,
+        }
+      );
+      if (res.data.success) {
+        setPublish(!publish);
+        toast.success(res.data.message);
       }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   return (
     <Card>
@@ -138,30 +180,60 @@ const CourseTab = () => {
             Make changes to your courses here. Click save when you're done.
           </CardDescription>
         </div>
-        <div className='space-x-2'>
-          <Button onClick={()=>togglePublishUnpublish(selectedCourse.isPublished ? "false" : "true")} className="bg-gray-800">{selectedCourse.isPublished ? "UnPublish" : "Publish"}</Button>
-          <Button variant="destructive">remove Course</Button>
+        <div className="space-x-2">
+          <Button
+            onClick={() =>
+              togglePublishUnpublish(
+                selectedCourse.isPublished ? "false" : "true"
+              )
+            }
+            className="bg-gray-800"
+          >
+            {selectedCourse.isPublished ? "UnPublish" : "Publish"}
+          </Button>
+          <Button
+            variant="destructive"
+            onClick={deleteCourseHandler}
+            disabled={loading}
+          >
+            {loading ? "Removing..." : "Remove Course"}
+          </Button>
         </div>
       </CardHeader>
       <CardContent>
-        <div className='space-y-4 mt-5' >
+        <div className="space-y-4 mt-5">
           <div>
             <Label>Title</Label>
-            <Input value={input.courseTitle} onChange= { changeEventHandler} type="text" name="courseTitle" placeholder="Ex. Fullstack developer"></Input>
+            <Input
+              value={input.courseTitle}
+              onChange={changeEventHandler}
+              type="text"
+              name="courseTitle"
+              placeholder="Ex. Fullstack developer"
+            ></Input>
           </div>
           <div>
             <Label>Subtitle</Label>
-            <Input value={input.subTitle} onChange={changeEventHandler} type="text" name="subTitle" placeholder="Ex. Become a fullstack developer from zero to hero in 2 months"></Input>
+            <Input
+              value={input.subTitle}
+              onChange={changeEventHandler}
+              type="text"
+              name="subTitle"
+              placeholder="Ex. Become a fullstack developer from zero to hero in 2 months"
+            ></Input>
           </div>
           <div>
             <Label>Description</Label>
-            <RichTextEditor input={input} setInput={setInput}/>
+            <RichTextEditor input={input} setInput={setInput} />
             {/* <Input input={input} setInput={setInput} type="text" name="Description" placeholder="Ex. Description course"></Input> */}
           </div>
-          <div className='flex md:flex-row flex-wrap gap-1 items-center md:gap-5'>
+          <div className="flex md:flex-row flex-wrap gap-1 items-center md:gap-5">
             <div>
               <Label>Category</Label>
-              <Select defaultValue={input.category} onValueChange={selectCategory}>
+              <Select
+                defaultValue={input.category}
+                onValueChange={selectCategory}
+              >
                 <SelectTrigger className="w-[180px]">
                   <SelectValue placeholder="Select a category" />
                 </SelectTrigger>
@@ -170,9 +242,15 @@ const CourseTab = () => {
                     <SelectLabel>Category</SelectLabel>
                     <SelectItem value="Next Js">Next Js</SelectItem>
                     <SelectItem value="Data Science">Data Science</SelectItem>
-                    <SelectItem value="Frontend Development">Frontend Development</SelectItem>
-                    <SelectItem value="Backend Development">Backend Development</SelectItem>
-                    <SelectItem value="MernStack Development">MernStack Development</SelectItem>
+                    <SelectItem value="Frontend Development">
+                      Frontend Development
+                    </SelectItem>
+                    <SelectItem value="Backend Development">
+                      Backend Development
+                    </SelectItem>
+                    <SelectItem value="MernStack Development">
+                      MernStack Development
+                    </SelectItem>
                     <SelectItem value="Javascript">Javascript</SelectItem>
                     <SelectItem value="Python">Python</SelectItem>
                     <SelectItem value="Docker">Docker</SelectItem>
@@ -183,7 +261,10 @@ const CourseTab = () => {
             </div>
             <div>
               <Label>Course Level</Label>
-              <Select defaultValue={input.courseLevel} onValueChange={selectCourseLevel}>
+              <Select
+                defaultValue={input.courseLevel}
+                onValueChange={selectCourseLevel}
+              >
                 <SelectTrigger className="w-[180px]">
                   <SelectValue placeholder="Select a course level" />
                 </SelectTrigger>
@@ -200,49 +281,57 @@ const CourseTab = () => {
             <div>
               <Label>Price in (INR)</Label>
               <Input
-              type="number"
-              name="coursePrice"
-              value={input.coursePrice}
-              onChange={changeEventHandler}
-              placeholder="199"
-              className="w-fit"
+                type="number"
+                name="coursePrice"
+                value={input.coursePrice}
+                onChange={changeEventHandler}
+                placeholder="199"
+                className="w-fit"
               />
             </div>
           </div>
           <div>
-              <Label>Course Thumbnail</Label>
-              <Input
+            <Label>Course Thumbnail</Label>
+            <Input
               type="file"
               id="file"
               onChange={selectThumbnail}
               placeholder="199"
               accept="image/*"
               className="w-fit"
+            />
+            {previewThumbnail && (
+              <img
+                src={previewThumbnail}
+                alt="Thumbnail"
+                className="w-64 my-2"
               />
-              {
-                previewThumbnail && (
-                  <img src={previewThumbnail} alt="Thumbnail" className='w-64 my-2' />
-                )
-              }
-            </div>
-            <div className='flex gap-2'>
-              <Button onClick={()=> navigate('/admin/course')} variant="outline">Cancel</Button>
-              <Button className="bg-gray-800 " disabled={loading} onClick={updateCourseHandler}>
-                {
-                  loading ? (
-                    <>
-                      <Loader2 className='mr-2 w-4 h-4 animate-spin'>
-                        Please wait
-                      </Loader2>
-                    </>
-                  ):("Save")
-                }
-              </Button>
-            </div>
+            )}
+          </div>
+          <div className="flex gap-2">
+            <Button onClick={() => navigate("/admin/course")} variant="outline">
+              Cancel
+            </Button>
+            <Button
+              className="bg-gray-800 "
+              disabled={loading}
+              onClick={updateCourseHandler}
+            >
+              {loading ? (
+                <>
+                  <Loader2 className="mr-2 w-4 h-4 animate-spin">
+                    Please wait
+                  </Loader2>
+                </>
+              ) : (
+                "Save"
+              )}
+            </Button>
+          </div>
         </div>
       </CardContent>
     </Card>
-  )
-}
+  );
+};
 
-export default CourseTab
+export default CourseTab;
