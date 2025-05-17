27c0a428103ed75685a410c576c80aa4ab86ae20
buicongthanh861 <buicongthanh861@gmail.com>
2025-05-17 22:44:01 +0700

    Update documentSlice.js

--- Client/src/redux/documentSlice.js
+++ Client/src/redux/documentSlice.js
@@ -1,1 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const documentSlice = createSlice({
+  name: "document",
+  initialState: {
+    document: null, // chỉ lưu 1 document hiện tại
+  },
+  reducers: {
+    setDocument: (state, action) => {
+      state.document = action.payload;
+    },
+  },
+});
+
+export const { setDocument } = documentSlice.actions;
+export default documentSlice.reducer;
 
