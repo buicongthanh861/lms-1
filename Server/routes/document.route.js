1b39a91cd46e4bdd837884a7c7f299a13bec34ef
buicongthanh861 <buicongthanh861@gmail.com>
2025-05-17 22:29:13 +0700

    Update document.route.js

--- Server/routes/document.route.js
+++ Server/routes/document.route.js
@@ -1,1 +1,17 @@
+import express from "express";
+import { isAuthenticated } from "../middleware/isAuthenticated.js";
+import { createDocument, deleteDocument, getCourseDocuments } from "../controllers/document.controller.js";
+import { singleUpload } from "../middleware/multer.js";
 
+const router = express.Router();
+
+// Tạo document cho course cụ thể
+router.route("/:courseId/document").post(isAuthenticated, singleUpload, createDocument);
+
+// Lấy tất cả document của course cụ thể
+router.route("/:courseId/document").get(isAuthenticated, getCourseDocuments);
+
+// Xóa một document cụ thể
+router.route("/document/:documentId").delete(isAuthenticated, deleteDocument);
+
+export default router;
