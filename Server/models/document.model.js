d870ee3d84c94905491b307857ce6688c5507473
buicongthanh861 <buicongthanh861@gmail.com>
2025-05-17 22:28:22 +0700

    Update document.model.js

--- Server/models/document.model.js
+++ Server/models/document.model.js
@@ -1,1 +1,28 @@
+import mongoose from "mongoose";
+
+const DocumentSchema = new mongoose.Schema(
+  {
+    doctitle: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    fileUrl: {
+      type: String,
+      required: true,
+    },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Document = mongoose.model("Document", DocumentSchema);
 
