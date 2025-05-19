8dc9e9cc0f30ccd29b783b92ee4628f4b1597f85
buicongthanh861 <buicongthanh861@gmail.com>
2025-05-19 07:44:12 +0700

    Update CourseCard.jsx

--- Client/src/components/CourseCard.jsx
+++ Client/src/components/CourseCard.jsx
@@ -6,14 +6,16 @@ import { useSelector } from 'react-redux'
 
 const CourseCard = ({course}) => {
   const navigate = useNavigate()
-  const {user} = useSelector(store=>store.auth)
+  const {user} = useSelector(store => store.auth)
   return (
     <Card key={course._id} className="bg-white shadow-lg">
       <img src={course.courseThumbnail} alt="" className='w-full h-48 object-cover'/>
       <div className='p-6'>
         <h2 className='text-xl font-semibold text-gray-800 mb-3'>{course.courseTitle}</h2>
         <p className='text-gray-600 mb-4'>{course.subTitle}</p>
-        <Button onClick={()=>navigate(user ? `/courses/${course._id}`:"/login")}>Learn More</Button>
+        <Button onClick={() => navigate(user ? `/courses/${course._id}` : "/login")}>
+          Tìm hiểu thêm
+        </Button>
       </div>
     </Card>
   )
